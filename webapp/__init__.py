b7f3273248eadb027e54d602ad004adbd3bfdd3a
Mark Nixon <markwnixon@gmail.com>
2020-07-31 07:36:57 -0400

    from work

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from webapp.CCC_system_setup import scac, machine, statpath, dbp
 
-app = Flask(__name__, static_folder = "./static")
+app = Flask(__name__)
 
 ####################################################################
 ########## SET DATABASE STRUCTURES #################################
@@ -24,6 +24,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["DEBUG"] = True
 app.config["SECRET_KEY"] = dbp[5]
 app.secret_key = dbp[5]
+
+print(f'username:{dbp[1]},password:{dbp[2]},hostname:{dbp[3]},databasname:{dbp[4]}')
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
