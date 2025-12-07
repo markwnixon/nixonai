32af8115d02151cb94718ad686b9e81b823404f0
markwnixon@gmail.com <markwnixon@gmail.com>
2025-12-07 12:29:27 -0500

    inputs change

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 from webapp.CCC_system_setup import scac, machine, statpath, dbp
 
 from webapp.extensions import db, bcrypt, login_manager, jwt
-
+from webapp.routes import main
 from webapp.authenticate.routes import authenticate
 
 ####################################################################
@@ -43,7 +43,6 @@ def create_app():
 
 
     app.register_blueprint(authenticate)
-    from webapp.routes import main
     app.register_blueprint(main)
 
     return app
